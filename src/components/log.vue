fa686ea7488a32cbf73e0f0fe09e30b653b8d620
stefanonepa <stefano.nepa@epfl.ch>
2017-03-03 13:46:09 +0100

    It works on the phone. It's awesome.

---
+++ src/components/log.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- root node required -->
+  <div>
+    <!-- your content -->
+    <div class="layout-padding">
+      <!-- if you want automatic padding -->
+      hello toto
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
