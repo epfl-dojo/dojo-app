b1de2d7e4355d75dde313de8d723de4864c1c755
ponsfrilus <ponsfrilus@gmail.com>
2017-03-03 16:11:53 +0100

    Links from dojo-data :fire:

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -5,14 +5,32 @@
     <div class="layout-padding">
       <!-- if you want automatic padding -->
       Here comes a bunch of links
+      <ul id="dojo-links">
+        <li v-for="link in links">
+          <a :href="link.link_url">{{ link.link_title }}</a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  components: {
+    //LinksHolder
+  },
   data () {
-    return {}
+    return {
+      links: []
+    }
+  },
+  created () {
+    axios.get('https://raw.githubusercontent.com/epfl-dojo/dojo-data/master/src/links.json')
+    .then((response) => {
+      console.log(response.data)
+      this.links = response.data.links
+    })
   }
 }
 </script>
