c84eb4d9f10792405f31164a49bb9f03dce9eb1c
ponsfrilus <ponsfrilus@gmail.com>
2017-03-14 22:11:42 +0100

    About skeleton with list of repos

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,16 @@
     <!-- your content -->
     <div class="layout-padding">
       <!-- if you want automatic padding -->
-      Here comes the story of this app. <a href="https://github.com/epfl-dojo">EPFL-Dojo</a> FTW !
+      <h3>About EPFL dojo</h3>
+
+      <h3>About this app</h3>
+
+      <h3>EPFL dojo repositories</h3>
+      <ul>
+        <li v-for="repo in dojoRepos">
+           <a :href="repo.html_url">{{ repo.full_name }}</a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -12,7 +21,18 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      dojoRepos: [],
+      dojoContributors: []
+    }
+  },
+  created () {
+    // Getting the list of epfl-dojo repositories
+    this.$http.get('https://api.github.com/orgs/epfl-dojo/repos')
+    .then((response) => {
+      this.dojoRepos = response.data
+      return this.dojoRepos
+    })
   }
 }
 </script>
